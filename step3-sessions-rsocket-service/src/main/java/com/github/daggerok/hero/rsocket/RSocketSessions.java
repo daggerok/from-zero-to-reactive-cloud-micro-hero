d6c52551009ef2b0ee5f4e9d4e919cc25831c827
Maksim Kostromin <daggerok@gmail.com>
2019-10-24 03:29:49 +0300

    Like in article...

--- step3-sessions-rsocket-service/src/main/java/com/github/daggerok/hero/rsocket/RSocketSessions.java
+++ step3-sessions-rsocket-service/src/main/java/com/github/daggerok/hero/rsocket/RSocketSessions.java
@@ -2,16 +2,13 @@ package com.github.daggerok.hero.rsocket;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,8 +20,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class RSocketSessions {
 
-    private final DatabaseClient client;
-    private final ReactiveTransactionManager rtm;
+    // private final DatabaseClient client;
+    // private final ReactiveTransactionManager rtm;
     private final SessionRepository sessionRepository;
 
     @ResponseBody
@@ -65,38 +62,45 @@ public class RSocketSessions {
     //                .flatMap(sessionRepository::save);
     // }
 
-    /**
-     * Failed with: NoSuchBeanDefinitionException:
-     * No qualifying bean of type 'org.springframework.transaction.PlatformTransactionManager' available
-     */
-    @ResponseBody
-    @Transactional
-    @PostMapping("/sessions")
-    public Disposable save(@RequestBody Session session) {
-        return Mono.just(session)
-                   .filter(s -> Objects.nonNull(s.getName()))
-                   .filter(s -> Objects.nonNull(s.getSpeakers()))
-                   .map(s -> Objects.isNull(s.getId()) ? s.setId(UUID.randomUUID()) : s)
-                   .flatMap(sessionRepository::save)
-                   .subscribe(log::info);
-    }
-
     // /**
-    //  * Also doesn't work...
+    //  * Failed with: NoSuchBeanDefinitionException:
+    //  * No qualifying bean of type 'org.springframework.transaction.PlatformTransactionManager' available
     //  */
     // @ResponseBody
     // @Transactional
     // @PostMapping("/sessions")
-    // public Mono<Void> save(@RequestBody Session session) {
-    //     TransactionalOperator rtx = TransactionalOperator.create(rtm);
-    //     return rtx.execute(status -> Mono.just(session)
-    //                                      .filter(s -> Objects.nonNull(s.getName()))
-    //                                      .filter(s -> Objects.nonNull(s.getSpeakers()))
-    //                                      .map(s -> Objects.isNull(s.getId()) ? s.setId(UUID.randomUUID()) : s)
-    //                                      .flatMap(sessionRepository::save)
-    //                                      .then())
-    //               .then();
+    // public Disposable save(@RequestBody Session session) {
+    //     return Mono.just(session)
+    //                .filter(s -> Objects.nonNull(s.getName()))
+    //                .filter(s -> Objects.nonNull(s.getSpeakers()))
+    //                .map(s -> Objects.isNull(s.getId()) ? s.setId(UUID.randomUUID()) : s)
+    //                .flatMap(sessionRepository::save)
+    //                .subscribe(log::info);
     // }
+
+    // /**
+    //  * Also failing...
+    //  * @return
+    //  */
+    // @ResponseBody
+    // @Transactional
+    // @PostMapping("/sessions")
+    // public Disposable save(@RequestBody Session session) {
+    //     if (Objects.isNull(session.getName())) throw new IllegalStateException("name is required.");
+    //     if (Objects.isNull(session.getId())) session.setId(UUID.randomUUID());
+    //     TransactionalOperator rtx = TransactionalOperator.create(rtm);
+    //     return rtx.execute(status -> sessionRepository.save(session))
+    //               .subscribe();
+    // }
+
+    @ResponseBody
+    @Transactional
+    @PostMapping("/sessions")
+    public Mono<Session> save(@RequestBody Session session) {
+        if (Objects.isNull(session.getName())) throw new IllegalStateException("name is required.");
+        if (Objects.isNull(session.getId())) session.setId(UUID.randomUUID());
+        return sessionRepository.save(session);
+    }
 
     @MessageMapping("sessions")
     public Flux<Session> get() {
--- step3-sessions-rsocket-service/src/main/java/com/github/daggerok/hero/rsocket/SessionsRSocket2Application.java
+++ step3-sessions-rsocket-service/src/main/java/com/github/daggerok/hero/rsocket/SessionsRSocket2Application.java
@@ -8,32 +8,22 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.connectionfactory.R2dbcTransactionManager;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.reactive.TransactionalOperator;
 
 @SpringBootApplication
+@EnableR2dbcRepositories
 // @RequiredArgsConstructor
 @EnableTransactionManagement
 @EnableConfigurationProperties(PostgresProps.class)
 public class SessionsRSocket2Application /*extends AbstractR2dbcConfiguration */{
 
-    // private final ConnectionFactory connectionFactory;
-    //
-    // @Override
-    // public ConnectionFactory connectionFactory() {
-    //     return connectionFactory;
-    // }
-    //
-    // @Bean
-    // public ReactiveTransactionManager reactiveTransactionManager() {
-    //     return new R2dbcTransactionManager(connectionFactory);
-    // }
-    //
-    // @Bean
-    // public TransactionalOperator transactionalOperator() {
-    //     return TransactionalOperator.create(reactiveTransactionManager());
-    // }
+    @Bean
+    public ReactiveTransactionManager reactiveTransactionManager(ConnectionFactory cf) {
+        return new R2dbcTransactionManager(cf);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SessionsRSocket2Application.class, args);
